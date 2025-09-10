15efecdcabd0ade0f509cbce5e3493efd66fd829
Tito Mwenda <titomwenda27@gmail.com>
2025-09-10 06:36:45 +0300

    Add files via upload
    I have joined using INNER JOIN, LEFT JOIN, and RIGHT JOIN with the USING CLAUSE because it's efficient. I have used the LIMIT clause to restrict the number of rows to 10.

---
+++ answers.sql
@@ -0,0 +1,22 @@
+USE salesdb;
+
+-- 1. Write an SQL query to get the firstName, lastName, email, and officeCode of all employees.
+-- Use an INNER JOIN to combine the employees table with the offices table using the officeCode column.
+   SELECT e.firstName, e.lastName, e.email, o.officecode
+FROM employees as e
+INNER JOIN offices as o on e.officecode = o.officeCode;
+
+-- 2. Write an SQL query to get the productName, productVendor, and productLine from the products table.
+-- Use a LEFT JOIN to combine the products table with the productlines table using the productLine column.
+    SELECT p.productName, p.productVendor, pl.productline
+FROM Products as p
+LEFT JOIN productlines as pl using (productline);
+
+-- 3. Write an SQL query to retrieve the orderDate, shippedDate, status, and customerNumber for the first 10 orders.
+-- Use a RIGHT JOIN to combine the customers table with the orders table using the customerNumber column.
+    SELECT	o.orderDate, o.shippedDate, o.status, c.customerNumber
+FROM orders as o
+RIGHT JOIN customers as c USING (customerNumber) 
+LIMIT 10;
+
+
